b9e17febd00e853c0de04151801fed9bfebb2a5a
tamerlan1wow <slatergt228@gmail.com>
2024-10-09 14:42:37 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,14 +6,23 @@ const plus = document.getElementById("butplus");
 const minus = document.getElementById("butminus");
 const mult = document.getElementById("butmult");
 const div = document.getElementById("butdiv");
-
-const first1 = document.getElementById("first1");
-const second1 = document.getElementById("second1");
+const cos = document.getElementById("butcos");
+const sin = document.getElementById("butsin");
+const tg = document.getElementById("buttg");
+const ctg = document.getElementById("butctg");
+let first = document.getElementById("first");
+let second = document.getElementById("second");
+let va = document.getElementById("va");
 const result1 = document.getElementById("result1");
+const result2 = document.getElementById("result2");
+
+let res = 0;
+let res2 = 0;
+
 
 function basicCalculus(mark) {
-    const num1 = parseFloat(first1.value) || 0;
-    const num2 = parseFloat(second1.value) || 0;
+    const num1 = parseFloat(first.value) || 0;
+    const num2 = parseFloat(second.value) || 0;
     let res;
 
     switch (mark) {
@@ -54,59 +63,89 @@ div.addEventListener("click", (event) => {
     basicCalculus("/");
 });
 
-const cos = document.getElementById("butcos");
-const sin = document.getElementById("butsin");
-const tg = document.getElementById("buttg");
-const cotan = document.getElementById("butctg");
+function calculus(mark) {
+    const num1 = parseFloat(first.value) || 0;
+    const num2 = parseFloat(second.value) ||0;
+    switch(mark) {
+        case "+":
+            res = num1 + num2;
+            break;
+        case "-":
+            res = num1 - num2;
+            break;
+        case "*":
+            res = num1 * num2;
+            break;
+        case "/":
+            res = num1 / num2;
+            break;
+    }
+    result.textContent = res;
+}
 
-const first2 = document.getElementById("first2");
-const result2 = document.getElementById("result2");
+plus.addEventListener("click", (event) => {
+    event.preventDefault()
+    calculus("+");
+});
 
-function trigCalculus(mark) {
-    const num = parseFloat(first2.value) || 0;
-    let val;
-    
-    switch (mark) {
+minus.addEventListener("click", (event) => {
+    event.preventDefault()
+    calculus("-");
+});
+
+mult.addEventListener("click", (event) => {
+    event.preventDefault()
+    calculus("*");
+});
+
+div.addEventListener("click", (event) => {
+    event.preventDefault()
+    calculus("/");
+});
+
+function tri(trig) {
+    const num = parseFloat(va.value) || 0;
+    num_c = num*Math.PI/180;
+    switch(trig) {
         case "cos":
-            val = Math.cos(num);
-            val.toFixed(2)
+            res2 = Math.cos(num_c);
             break;
         case "sin":
-            val = Math.sin(num);
-            val.toFixed(2)
+            res2 = Math.sin(num_c);
             break;
         case "tg":
-            val = (num % Math.PI) === (Math.PI / 2) ? "error" : Math.tan(num);
-            val.toFixed(2)
+            res2 = (num_c % Math.PI) === (Math.PI/2) ? "error" : Math.tg(num_c);
             break;
         case "ctg":
-            val = (num % Math.PI) === 0 ? "error" : 1 /(Math.tan(num));
-            val.toFixed(2)
+            res2 = (num_c % Math.PI) === 0 ? "error" : 1/(Math.tg(num_c));
             break;
-        default:
-            val = "Invalid operation";
     }
 
-    result2.textContent = val;
+    if(res2 === "error"){
+        result2.textContent = res2;
+    }
+    else {
+    result2.textContent = res2.toFixed(2);
+    }
 }
 
 cos.addEventListener("click", (event) => {
-    event.preventDefault();
-    trigCalculus("cos");
+    event.preventDefault()
+    tri("cos");
 });
 
 sin.addEventListener("click", (event) => {
-    event.preventDefault();
-    trigCalculus("sin");
+    event.preventDefault()
+    tri("sin");
 });
 
-tg.addEventListener("click", (event) => {
-    event.preventDefault();
-    trigCalculus("tg");
+tan.addEventListener("click", (event) => {
+    event.preventDefault()
+    tri("tg");
 });
 
-cotan.addEventListener("click", (event) => {
-    event.preventDefault();
-    trigCalculus("ctg");
+cot.addEventListener("click", (event) => {
+    event.preventDefault()
+    tri("ctg");
 });
 
